d5ab41cd6f5ff517a4f2beaa6d6cd82962880d79
Just-A-Pixel <raj.anand0511@gmail.com>
2025-10-15 01:32:36 +0530

    refactor(chatbox): Separated tailwind logic

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -1,14 +1,10 @@
 import type { JSX } from "react/jsx-runtime";
+import ChatBoxLayout from "./ui/ChatBoxLayout";
 
 export default function ChatBox({ text }: { text: string }): JSX.Element {
   return (
-    <div className="flex">
-      <div className="flex-none w-10 h-10 rounded-full bg-indigo-100 flex items-center justify-center text-indigo-600 font-semibold">U</div>
-      <div className="ml-3">
-        <div className="inline-block bg-indigo-50 border border-indigo-100 text-slate-800 p-3 rounded-lg max-w-xl whitespace-pre-wrap">
+    <ChatBoxLayout>
           {text}
-        </div>
-      </div>
-    </div>
+    </ChatBoxLayout>
   )
 }
---
+++ frontend/src/components/ui/ChatBoxLayout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { JSX } from "react/jsx-runtime";
+
+export default function ChatBoxLayout(props: { children: React.ReactNode }): JSX.Element {
+  return (
+   <div className="flex">
+   <div className="flex-none w-10 h-10 rounded-full bg-indigo-100 flex items-center justify-center text-indigo-600 font-semibold">U</div>
+   <div className="ml-3">
+     <div className="inline-block bg-indigo-50 border border-indigo-100 text-slate-800 p-3 rounded-lg max-w-xl whitespace-pre-wrap">
+       {props.children}
+     </div>
+   </div>
+ </div>
+  );
+}
